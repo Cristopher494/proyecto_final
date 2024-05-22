bd022e24ff017d082897cf5629898284425b70c8
Cristopher Rojas <crrojasgo@gmail.com>
2024-05-22 11:59:05 +0200

    updated1

---
+++ inicio.py
@@ -0,0 +1,45 @@
+import streamlit as st
+from PIL import Image
+import requests
+import numpy as np
+from tensorflow.keras.models import load_model
+from streamlit_lottie import st_lottie
+from utils import load_ben_color
+import time
+from streamlit_extras.switch_page_button import switch_page
+
+# Cargar el modelo
+modelo = load_model('./project/models/densenet121_model.h5')
+
+def load_lottie_url(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+# URL de la animación Lottie
+lottie_coding = load_lottie_url("https://lottie.host/132a64cd-b421-46f4-99ec-8387c1ced1af/4L0M7FMGWD.json")
+
+# Página principal
+def main_page():
+    st.title("Detección de Retinopatía Diabética")
+    st_lottie(lottie_coding, height=300, key='coding')
+    st.write('<h4 style="font-size: 24px;">Desde el Centro de Oftalmología Geeks ponemos a disposición el Sistema de Diagnóstico Automático de Rinopatía</h4>', unsafe_allow_html=True)
+    if st.button("Realizar diagnóstico inteligente"):
+        switch_page("Diagnóstico Inteligente")
+    
+    st.write("---")
+
+    st.subheader("¿Qué es la retinopatía diabética?")
+    st.image("./pages/Animation/Untitled_Project_V2.gif", use_column_width=True)
+    st.write("La retina es la capa del fondo del ojo que recibe los estímulos luminosos y percibe las imágenes que serán enviadas a nuestro cerebro")
+    st.write("Al igual que en el resto de complicaciones crónicas de la diabetes mellitus, **la presencia de niveles elevados de glucosa en sangre durante muchos años, produce alteraciones en los vasos sanguíneos de la retina que originan daño en este importante tejido ocular.**")
+    st.write("Dichas alteraciones vasculares dificultan el aporte de oxígeno a la retina, en la cual se producen microaneurismas, hemorragias, así como fuga de lípidos y proteínas desde el interior de los capilares dañados. Esto ocurre fundamentalmente cuando la diabetes no se controla correctamente y recibe el nombre de retinopatía diabética.")
+    st.write("Estos cambios son progresivos y la falta de oxigenación de la retina estimula el crecimiento de más vasos que intentan suplir el déficit de aporte sanguíneo. La aparición de estos nuevos vasos (fase denominada retinopatía diabética proliferante) puede producir daños irreversibles en la retina")
+
+# Sistema de navegación
+def main():
+    main_page()
+
+if __name__ == "__main__":
+    main()
---
+++ pages/Diagnóstico Inteligente.py
@@ -0,0 +1,96 @@
+import streamlit as st
+from PIL import Image
+import requests
+import numpy as np
+from tensorflow.keras.models import load_model
+from streamlit_lottie import st_lottie
+import time
+from streamlit_extras.switch_page_button import switch_page
+from utils import load_image, load_ben_color
+
+# Cargar el modelo
+modelo = load_model('./project/models/densenet121_model.h5')
+
+# Función para cargar la animación Lottie
+def load_lottie_url(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+# URL de la animación Lottie
+lottie_coding = load_lottie_url("https://lottie.host/132a64cd-b421-46f4-99ec-8387c1ced1af/4L0M7FMGWD.json")
+
+# Página de diagnóstico
+def diagnosis_page():
+    # Botón para volver a la página principal
+    if st.button("Volver al inicio"):
+        switch_page("Inicio")
+    
+    st.title("Diagnóstico Inteligente")
+
+    # Sección de datos del usuario
+    st.subheader("Por favor, ingrese sus datos:")
+    
+    gender = st.selectbox("Género", ["Femenino", "Masculino"])
+    age = st.text_input("Edad", max_chars=2)
+    diabetes_diagnosis = st.selectbox("Diagnóstico de Diabetes", ["Sí", "No"])
+    
+    st.write("---")
+
+    st.subheader("¿Qué imagen tengo que cargar?")
+    st.write("Para que el sistema devuelva una predicción acertada, es necesario cargar una imagen obtenida a partir de una ***Retinografía o Imagen de Fondo de Ojo***")
+
+    st.subheader("¿Qué resultados obtendré?")
+    st.write('<ul><li><b>No se detecta retinopatía diabética</b>: No se han podido identificar signos de rinopatía</li>', unsafe_allow_html=True)
+    st.write('<ul><li><b>Se sugiere evaluación profesional</b>Detección moderada de signos de Rinopatía, se sugiere profundizar el diagnóstico con un profesional</li>', unsafe_allow_html=True)
+    st.write('<ul><li><b>Retinopatía diabética detectada</b>: Evidente detección de signos de Rinopatía: se sugiere realizar consulta profesional cuanto antes.</li>', unsafe_allow_html=True)
+    
+    # Carga de imagen
+    image_file = st.file_uploader("Carga una imagen", type=["jpg", "jpeg", "png"])
+        
+    if image_file is not None:
+        # Mostrar la imagen cargada
+        img = Image.open(image_file)
+        
+        # Botón para activar la predicción
+        if st.button("Realizar Predicción"):
+            # Crear un marcador de posición para la animación
+            placeholder = st.empty()
+           
+            with st.spinner('Procesando la imagen...'):
+                # Mostrar la animación de procesamiento
+                with placeholder:
+                    st_lottie(lottie_coding, height=100, key='processing')
+                    time.sleep(8)  # Pausar por 2 segundos (ajustar según sea necesario)
+
+                # Preprocesar la imagen
+                img_array = np.array(img)
+                processed_image = load_ben_color(img_array)
+                processed_image = np.expand_dims(processed_image, axis=0)
+                       
+                # Realizar la predicción con el modelo
+                prediction = modelo.predict(processed_image)
+                probability = prediction[0][0]
+
+                # Ocultar la animación después del procesamiento
+                placeholder.empty()
+
+                # Mostrar el resultado de la predicción
+                st.write("Resultado de la predicción:")
+                st.write(probability)
+
+                if probability < 0.2:
+                    st.info("No se detecta retinopatía diabética" )
+                elif 0.2 <= probability < 0.4:
+                    st.warning("Se sugiere evaluación profesional")
+                else:
+                    st.error("Retinopatía diabética detectada")   
+        
+        st.image(img, caption="Imagen cargada", use_column_width=True)
+
+    st.write("---")
+
+# Ejecutar la página de diagnóstico
+if __name__ == "__main__":
+    diagnosis_page()
